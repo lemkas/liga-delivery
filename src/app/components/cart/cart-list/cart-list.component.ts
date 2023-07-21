b4fb90c1d364aa9aa5592ef08e094291087c6162
lemov nikita <lemov1980@yandex.ru>
2023-07-21 12:33:47 +0300

    rewrite cart service to subject

--- src/app/components/cart/cart-list/cart-list.component.ts
+++ src/app/components/cart/cart-list/cart-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ICartItem } from 'src/app/interfaces/cart-item';
 import { CartService } from 'src/app/services/cart.service';
 import { MatDialog } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'cart-list',
@@ -11,17 +12,19 @@ import { MatDialog } from '@angular/material/dialog';
 export class CartListComponent implements OnInit {
   constructor(private cartService: CartService, private dialog: MatDialog) {}
   cartItems: ICartItem[] = [];
+  private sub$!: Subscription;
   ngOnInit(): void {
     this.getCartItems();
   }
 
   private getCartItems(): void {
-    this.cartItems = this.cartService.getCartItems();
+    this.sub$ = this.cartService.sub$.subscribe((items) => {
+      this.cartItems = items;
+    });
     console.log(this.cartItems);
   }
 
   deleteCartItem(id: string): void {
     this.cartService.deleteCartItem(id);
-    this.getCartItems();
   }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { ICartItem } from '../interfaces/cart-item';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
   public cartItems: ICartItem[] = [];
+  sub$ = new BehaviorSubject<ICartItem[]>([]);
   constructor() {}
 
   addToCart(cartItem: ICartItem): string {
     this.cartItems.push(cartItem);
+    this.sub$.next(this.cartItems);
     return 'Блюдо добавлено в корзину';
   }
 
@@ -20,6 +23,7 @@ export class CartService {
 
   deleteCartItem(cartItemId: string) {
     this.cartItems = this.cartItems.filter((item) => item.id !== cartItemId);
+    this.sub$.next(this.cartItems);
     console.log(this.cartItems);
   }
 
